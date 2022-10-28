db1238e0470aee900043dcf297448f03e732c789
Madhur <madhur12a16072@gmail.com>
2022-10-28 11:38:20 +0530

    Desing changed

--- app/src/main/java/com/example/internfactory/Home_page.kt
+++ app/src/main/java/com/example/internfactory/Home_page.kt
@@ -4,12 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 
 class Home_page : Activity() {
 
     fun connect(view : View){
-        val intent=Intent(this,connecting::class.java)
+        val intent=Intent(this, connecting::class.java)
         startActivity(intent)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/internfactory/connecting.kt
+++ app/src/main/java/com/example/internfactory/connecting.kt
@@ -1,14 +1,11 @@
 package com.example.internfactory
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import kotlin.math.log
 
 class connecting : AppCompatActivity() {
 
